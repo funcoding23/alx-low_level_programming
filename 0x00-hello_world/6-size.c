ba7fb899a9a4014cfa2457f1e4c5c1b7cf2439c3
Ouijdane BOUOUZROU <ouijdane.bououzrou@gmail.com>
2023-04-20 18:14:08 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-  *main - Entry point 
-  *return: always 0 (Success)
-  */
+ *main-Entry point 
+ *return: always0(Success)
+ */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)", sizeof(char));
