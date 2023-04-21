7a28061dbaac7ec5c80f53c3dc1f529b438c9760
Ouijdane BOUOUZROU <ouijdane.bououzrou@gmail.com>
2023-04-20 19:47:03 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,11 @@
  *Return: Always 0 (Success)
 */
 int main(void)
-{
+{char a;
+int b;
+long int c;
+long long int d;
+float f;
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: lud byte(s)\n", sizeof(long int));
