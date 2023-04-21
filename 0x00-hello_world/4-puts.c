296fa27fe4cce45d44b74f583150739d60b31bc8
Ouijdane BOUOUZROU <ouijdane.bououzrou@gmail.com>
2023-04-20 18:38:28 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /**
-  *main - Entry point
-  *return: always 0 (success)
-  */
+ *main - Entry point
+ *
+ *Description: C program that prints with puts function
+ *
+ *return: always 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
