085a9c994f0e049f549af8278d2f2fce8c42e48a
Ouijdane BOUOUZROU <ouijdane.bououzrou@gmail.com>
2023-04-22 12:31:30 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	ld = %10;
+	ld = n % 10;
 	if (ld > 5)
 		printf("Last digit of %d id %d and is greater than 5\n", n, ld);
 	else if (ld == 0)
