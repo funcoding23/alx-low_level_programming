2fbfaa0e50c661187d714c370fc8ffc1a90f3548
Ouijdane BOUOUZROU <ouijdane.bououzrou@gmail.com>
2023-04-25 05:22:29 -0700

    4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,8 +7,7 @@
  *
  *return: returns 1 if c is lowercase
  * 	otherwise always 0 (success)
- */
-
+*/
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isalpha - check character both lowercase or uppercase
+ *
+ * @c: takes input from other functions.
+ *
+ * return: 1 is c if true else 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
