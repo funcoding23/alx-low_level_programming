d2251b437bbf618a1d96101a4ac81ff70da1009f
Ouijdane BOUOUZROU <ouijdane.bououzrou@gmail.com>
2023-04-25 14:00:34 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * return: sum of a & b
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
